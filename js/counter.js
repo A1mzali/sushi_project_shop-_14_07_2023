ef35c98ab2de5c3fcfc3aa4a3ca82dcec8dfe033
Aliia <112251947+A1mzali@users.noreply.github.com>
2023-07-15 20:32:41 +0300

    counter

--- js/counter.js
+++ js/counter.js
@@ -1,25 +1,33 @@
-// Находим элементы на странице
-const btnMinus = document.querySelector('[data-action="minus"]');
-const btnPlus = document.querySelector('[data-action="plus"]');
+// Добавляем прослушку на всем окне
 
-const counter = document.querySelector('[data-counter]');
+window.addEventListener('click', function (event) {
 
-// Отслеживаем клик на кнопку Минус
-btnMinus.addEventListener('click', function() {
-  console.log("minus click");
+  // Обьявляем переменную для счетчика
+  let counter;
 
-  if (parseInt(counter.innerText) > 1 ) {
-    // Изменяем текст в счетчике его на 1
-    counter.innerText = --counter.innerText;
-  } 
-  
-})
+  // проверяем клик строго по кнопкам + или -
+  if(event.target.dataset.action === 'plus'|| event.target.dataset.action === 'minus') {
+       // Находим обертку всего нашего счетчика
+       const counterWrapper = event.target.closest('.counter-wrapper');
 
-// Отслеживаем клик на кнопку Plus
+       // Находим див с числом счетчика
+        counter = counterWrapper.querySelector('[data-counter]')
+  }
+
+  // Провеоряем яв-ля ли элемент по которому был совершен клик кнопкой + или -
+  if (event.target.dataset.action === 'plus') {
+    counter.innerText = ++counter.innerText;
+  }
+
+  if (event.target.dataset.action === 'minus') {
+    if (parseInt(counter.innerText) > 1 ) {
+      // Изменяем текст в счетчике его на 1
+      counter.innerText = --counter.innerText;
+    } 
+    
+  }
 
 
 
-btnPlus.addEventListener('click', function() {
-  console.log("plus click");
-  counter.innerText = ++counter.innerText;
+
 })
